d0cf6d6f452125c522aa307bc80500f8d6e770e5
seikochan <j.s.ishigami@gmail.com>
2014-03-16 16:23:58 -1000

    Renamed locales to match Transifex language codes.

--- config.rb
+++ config.rb
@@ -44,7 +44,7 @@
 #     "Helping"
 #   end
 # end
-activate :i18n, :langs => [:en, :fr, :es, :pt, :it, :de, :nl, :el, :ru, :zh_HANS, :zh_HANT, :ja]
+activate :i18n, :langs => [:en, :fr, :es, :pt, :it, :de, :nl, :el, :ru, :zh_CN, :zh_TW, :ja]
 
 set :css_dir, 'stylesheets'
 
